c2c54cf88718228965ca3dd9a521f23439e134d5
Valeria310 <78411807+Valeria310@users.noreply.github.com>
2021-09-07 10:52:43 -0300

    feat: implement task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(matrix) {
+  let cats = 0;
+  let found = "^^";
+  for (i = 0; i < matrix.length; i++) {
+    for (j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] == found) {
+        cats++;
+      }
+    }
+  }
+  return cats;
 }
