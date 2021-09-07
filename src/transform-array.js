03df480e86d4811f6c8065999ff9859b04bfc1fc
Valeria310 <78411807+Valeria310@users.noreply.github.com>
2021-09-07 11:37:03 -0300

    feat: implement task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,31 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  let copyArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == `--discard-next`) {
+      copyArr.push(undefined);
+      i++;
+    } else if (arr[i] == `--discard-prev`) {
+      copyArr.pop();
+    } else if (arr[i] == `--double-next`) {
+      if (i + 1 < arr.length) {
+        copyArr.push(arr[i + 1]);
+      }
+    } else if (arr[i] == `--double-prev`) {
+      if (copyArr.length != 0) {
+        copyArr.push(copyArr[copyArr.length - 1]);
+      }
+    } else {
+      copyArr.push(arr[i]);
+    }
+  }
+  let newArr = [];
+  for (let i = 0; i < copyArr.length; i++) {
+    if (copyArr[i] != undefined) {
+      newArr.push(copyArr[i]);
+    }
+  }
+  return newArr;
 }
