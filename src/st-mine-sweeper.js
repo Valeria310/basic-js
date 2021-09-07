6cf3d4c2d1e2f7457af2907d85aaf0f453421ba6
Valeria310 <78411807+Valeria310@users.noreply.github.com>
2021-09-07 13:20:50 -0300

    feat: implement task

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,78 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper (matrix) {
+  const matrixCopy = new Array(matrix.length);
+
+  for (let i = 0; i < matrixCopy.length; i++) {
+    matrixCopy[i] = new Array(matrix[0].length);
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      matrixCopy[i][j] = 0;
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === true) {
+        if (i === 0
+          && j === 0) {
+          matrixCopy[i][j + 1]++;
+          matrixCopy[i + 1][j]++;
+          matrixCopy[i + 1][j + 1]++;
+        } else if (i === 0
+          && j === matrixCopy[0].length - 1) {
+          matrixCopy[i][j - 1]++;
+          matrixCopy[i + 1][j - 1]++;
+          matrixCopy[i + 1][j]++;
+        } else if (i === matrixCopy.length - 1
+          && j === 0) {
+          matrixCopy[i - 1][j]++;
+          matrixCopy[i - 1][j + 1]++;
+          matrixCopy[i][j + 1]++;
+        } else if (
+          i === matrixCopy.length - 1
+          && j === matrixCopy[0].length - 1
+        ) {
+          matrixCopy[i - 1][j - 1]++;
+          matrixCopy[i - 1][j]++;
+          matrixCopy[i][j - 1]++;
+        } else if (i === 0) {
+          matrixCopy[i][j - 1]++;
+          matrixCopy[i][j + 1]++;
+          matrixCopy[i + 1][j - 1]++;
+          matrixCopy[i + 1][j]++;
+          matrixCopy[i + 1][j + 1]++;
+        } else if (i === matrixCopy.length - 1) {
+          matrixCopy[i - 1][j - 1]++;
+          matrixCopy[i - 1][j]++;
+          matrixCopy[i - 1][j + 1]++;
+          matrixCopy[i][j - 1]++;
+          matrixCopy[i][j + 1]++;
+        } else if (j === 0) {
+          matrixCopy[i - 1][j]++;
+          matrixCopy[i - 1][j + 1]++;
+          matrixCopy[i][j + 1]++;
+          matrixCopy[i + 1][j]++;
+          matrixCopy[i + 1][j + 1]++;
+        } else if (j === matrixCopy[0].length - 1) {
+          matrixCopy[i - 1][j - 1]++;
+          matrixCopy[i - 1][j]++;
+          matrixCopy[i][j - 1]++;
+          matrixCopy[i + 1][j - 1]++;
+          matrixCopy[i + 1][j]++;
+        } else {
+          matrixCopy[i - 1][j - 1]++;
+          matrixCopy[i - 1][j]++;
+          matrixCopy[i - 1][j + 1]++;
+          matrixCopy[i][j - 1]++;
+          matrixCopy[i][j + 1]++;
+          matrixCopy[i + 1][j - 1]++;
+          matrixCopy[i + 1][j]++;
+          matrixCopy[i + 1][j + 1]++;
+        }
+      }
+    }
+  }
+  return matrixCopy;
 }
